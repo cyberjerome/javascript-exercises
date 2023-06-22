cfbba4f2fee41413c2d9b3fe4798949c662105a3
Jerome Mamauag <mamauag.jd@gmail.com>
2023-06-22 23:11:30 +0800

    Completed javascript exercises 8-11 of The Odin Project

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,13 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  // make lowercase
+  formattedStr = str.replace(/[^a-zA-Z0-9]/g, '').replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, '').toLowerCase();
+
+  // check if formatted string is equal to its reverse
+  return formattedStr === formattedStr.split('').reverse().join('');
+
 
 };
+
 
 // Do not edit below this line
 module.exports = palindromes;
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+  n =parseInt(n)
+    if (isNaN(n) || n <= 0) {
+    return "OOPS";
+  }
+  if (n === 1 || n === 2) {
+    return 1;
+  }
+  return fibonacci(n - 1) + fibonacci(n - 2);
 };
 
 // Do not edit below this line
 module.exports = fibonacci;
+
